bba7895b635ffdd171f9b08a9ef623c6891976c3
pmaksimov <vur21@ya.ru>
2019-08-27 18:42:39 +0300

    изменена версия

--- tapioca/__init__.py
+++ tapioca/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 __author__ = 'Filipe Ximenes'
 __email__ = 'filipeximenes@gmail.com'
-__version__ = '2019.4.5'
+__version__ = '2019.8.27'
 
 
 from .adapters import (
